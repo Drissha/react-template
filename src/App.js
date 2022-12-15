371e89d4313a16d324b69f29599413c61ebbcbb3
Rian Alfazri Khoirulloh <76834537+Drissha@users.noreply.github.com>
2022-12-15 19:29:44 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Header from "./components/header";
+import Footer from "./components/footer";
+// import Sidebar from "./components/sidebar";
+import Home from "./components/pages/home";
+import About from "./components/pages/about";
+import Services from "./components/pages/services";
+import Contact from "./components/pages/contact";
+import Dashboard from "./components/pages/user/dashboard";
+import Docs from "./components/pages/docs";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="clearfix">
+          <div className="contentArea text-center">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route path="/services" component={Services} />
+              <Route exact path="/contact" component={Contact} />
+              <Route path="/user" component={Dashboard} />
+              <Route path="/docs" component={Docs} />
+              <Route
+                render={() => {
+                  return (
+                    <h1 className="text-center">
+                      404
+                      <br />
+                      Not Found
+                    </h1>
+                  );
+                }}
+              />
+            </Switch>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
